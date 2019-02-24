00bc2da4f235e7806489d77263df758d6f719b65
Sebastian Hallum Clarke <s@zibity.com>
2019-02-24 14:26:11 -0500

    Making display work well

---
+++ js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function () {
+  jQuery('#usa-map').vectorMap({
+    map: 'usa_en',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    borderColor: '#f3f6f6',
+    color: '#dfe1e0',
+    borderWidth: 10,
+    enableZoom: false,
+    showTooltip: true,
+    selectedColor: '#acadfe',
+    hoverColor: '#acadfe',
+    onRegionSelect: function (event, code, region) {
+      console.log(event, code, region)
+    }
+  })
+})
