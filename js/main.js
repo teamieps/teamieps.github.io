46faee71499e92df9a106109cc221a4145980399
Sebastian Hallum Clarke <s@zibity.com>
2019-02-26 17:21:14 -0500

    Starting resources

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+let jQuery = window.jQuery
+
 jQuery(document).ready(function () {
+  setUpUSAMap()
+  console.log(database)
+})
+
+let setUpUSAMap = function () {
   jQuery('#usa-map').vectorMap({
     map: 'usa_en',
     backgroundColor: 'rgba(0, 0, 0, 0)',
@@ -11,8 +18,8 @@ jQuery(document).ready(function () {
     hoverColor: '#acadfe',
     onRegionSelect: function (event, code, region) {
       console.log(event, code, region)
+      window.alert('You selected ' + code)
     }
-  })
 
-  console.log(database)
-})
+  })
+}
