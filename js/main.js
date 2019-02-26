f1c13289e67a90dd4bf4ed87b849e7b79278e4d6
Sebastian Hallum Clarke <s@zibity.com>
2019-02-26 18:34:32 -0500

    Displaying resources

--- js/main.js
+++ js/main.js
@@ -26,8 +26,25 @@ let setUpUSAMap = function () {
   })
 }
 
+const resourceItemHTML = '<div class="resource-item"><h4></h4><p class="resource-description"></p></div>'
+
 const displayResources = function (stateCode) {
   const selectedStateCode = stateCode.toUpperCase(stateCode)
   const selectedStateName = states[selectedStateCode]
   jQuery('#state-name').text(selectedStateName)
+
+  const filteredResources = database.filter(entry => entry.State === selectedStateCode)
+
+  // Delete existing elements
+  jQuery('#resources-list').empty()
+
+  for (let resource of filteredResources) {
+    let resourceNode = jQuery(resourceItemHTML)
+    resourceNode.find('h4').text(resource['Organization Name'])
+    resourceNode.find('.resource-description').text(resource['Brief Description'])
+
+    jQuery('#resources-list').append(resourceNode)
+  }
+
+  console.log(filteredResources)
 }
