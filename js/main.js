4c2039c32a09d0878f5f42c3168261be0f043862
Sebastian Hallum Clarke <s@zibity.com>
2019-02-26 17:57:18 -0500

    Making state selection

--- js/main.js
+++ js/main.js
@@ -9,17 +9,25 @@ let setUpUSAMap = function () {
   jQuery('#usa-map').vectorMap({
     map: 'usa_en',
     backgroundColor: 'rgba(0, 0, 0, 0)',
-    borderColor: '#f3f6f6',
+    // borderColor: 'rgba(0, 0, 0, 0)',
+    borderColor: '#F4F6F6',
+    borderOpacity: 1,
     color: '#dfe1e0',
-    borderWidth: 10,
+    borderWidth: 2,
     enableZoom: false,
+    // showLabels: true,
     showTooltip: true,
     selectedColor: '#acadfe',
     hoverColor: '#acadfe',
     onRegionSelect: function (event, code, region) {
-      console.log(event, code, region)
-      window.alert('You selected ' + code)
+      displayResources(code)
     }
 
   })
+}
+
+const displayResources = function (stateCode) {
+  const selectedStateCode = stateCode.toUpperCase(stateCode)
+  const selectedStateName = states[selectedStateCode]
+  jQuery('#state-name').text(selectedStateName)
 }
